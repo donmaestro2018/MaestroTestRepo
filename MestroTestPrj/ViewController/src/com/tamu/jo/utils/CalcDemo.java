b855961ef096a581dccd95bc96ad01297e12bb1b
Abu Sha'ira IV <mabushaera@tamu.edu>
2018-03-25 17:55:21 -0500

    JIRA ISSUE #1 - Adding a the sum function

--- MestroTestPrj/ViewController/src/com/tamu/jo/utils/CalcDemo.java
+++ MestroTestPrj/ViewController/src/com/tamu/jo/utils/CalcDemo.java
@@ -2,6 +2,16 @@ package com.tamu.jo.utils;
 
 public class CalcDemo {
     public static void main(String[] args) {
+        CalcDemo demo = new CalcDemo();
+        System.out.println(demo.sum(5, -1));
+    }
 
+    /**
+     * @author Abu Shaira
+     * @date 25 Mar 2018
+     * @desc a function that sums up two integers.
+     */
+    public int sum(int num1, int num2) {
+        return num1 + num2;
     }
 }
