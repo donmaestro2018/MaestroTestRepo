7e91394559d6074d524e8e7afc89a20f17a920bc
Ashraf Al Hniti <ashrafmaestro2018@gmail.com>
2018-03-25 18:39:16 -0500

    removing the mistake I did by adding into dev directly

--- MestroTestPrj/ViewController/src/com/tamu/jo/utils/CalcDemo.java
+++ MestroTestPrj/ViewController/src/com/tamu/jo/utils/CalcDemo.java
@@ -16,12 +16,4 @@ public class CalcDemo {
         return num1 + num2;
     }
 
-    /**
-     * @author Ashraf
-     * @date 25 Mar 2018
-     * @desc a function that subtract two integers.
-     */
-    public int sub(int num1, int num2) {
-        return num1 - num2;
-    }
 }
